70dd9f1b2a7bf40d5f261d7c762420705488a83c
Kaito Kudou <kaitokudou2468110921@gmail.com>
2023-09-11 23:34:37 +0900

    [fix]リポジトリ候補を表示するListViewを配置し、タップ判定を追加

--- lib/ui/top_page.dart
+++ lib/ui/top_page.dart
@@ -9,6 +9,14 @@ class TopPage extends ConsumerStatefulWidget {
 }
 
 class _TopPageState extends ConsumerState<TopPage> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -21,12 +29,56 @@ class _TopPageState extends ConsumerState<TopPage> {
         ),
         body: Column(
           children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: 'リポジトリ名やキーワードを入力',
-                icon: Icon(Icons.search_sharp),
+            Container(
+              padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+              child: TextFormField(
+                controller: controller,
+                decoration: InputDecoration(
+                  hintText: 'リポジトリ名やキーワードを入力',
+                  icon: const Icon(Icons.search_sharp),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      controller.clear();
+                    },
+                    icon: const Icon(Icons.clear),
+                  ),
+                ),
               ),
             ),
+
+            const SizedBox(height: 24),
+
+            Expanded(
+              child: ListView.separated(
+                padding: const EdgeInsets.only(left: 16),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      // TODO: 詳細画面に遷移
+                      print('${index}');
+                    },
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      child: ListTile(
+                        title: Text(
+                          '${index}',
+                          style: const TextStyle(
+                              fontSize: 24
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider(
+                    height: 8.0,
+                  );
+                },
+
+              ),
+            )
           ],
         ),
       ),
